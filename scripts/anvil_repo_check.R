0ece1318a1f0cd467d55297c3656a2031025463d
jhudsl-robot <null>
2022-09-01 18:25:26 +0000

    🔄 Synced local 'scripts/anvil_repo_check.R' with remote 'scripts/anvil_repo_check.R'
    test-run

--- scripts/anvil_repo_check.R
+++ scripts/anvil_repo_check.R
@@ -5,6 +5,8 @@ library(httr)
 library(jsonlite)
 library(dplyr)
 library(readr)
+library(tidyr)
+library(stringr)
 
 option_list <- list(
   optparse::make_option(
@@ -20,38 +22,76 @@ opt_parser <- optparse::OptionParser(option_list = option_list)
 opt <- optparse::parse_args(opt_parser)
 git_pat <- opt$git_pat
 
-message(paste("Checking for AnVIL repositories..."))
+# --------- Set url and token ---------
 
-# Request search results specific to AnVIL within the jhudsl organization
-# and provide the appropriate GH token
-req <- httr::GET(
-  "https://api.github.com/search/repositories?q=GDSCN+user:jhudsl+OR+AnVIL+user:jhudsl",
-  httr::add_headers(Authorization = paste("token", git_pat))
-)
+message(paste("Querying Github API..."))
+
+# Request search results specific to jhudsl + fhdsl + DataTrail organizations
+# Also allows us to pull in repos forked into these organizations
+url <- "https://api.github.com/search/repositories?q=user:jhudsl+user:fhdsl+fork:true&per_page=50"
+
+# Provide the appropriate GH token & Make the request
+req <- GET(url = url, config = add_headers(Authorization = paste("token", git_pat)))
 
 if (!(httr::http_error(req))) {
   message(paste("API request successful!"))
+} else {
+  stop("API request failed!")
+}
+
+# --------- Traverse pages ---------
+
+# Pull out the last page number of the request
+last <- str_extract(req$headers$link, pattern = '.(?=>; rel=\"last\")')
+
+full_repo_df <- tibble()
+for (page in 1:last){
   
-  # Read in and save data
+  url <- paste0("https://api.github.com/search/repositories?q=user:jhudsl+user:fhdsl+fork:true&per_page=50&page=", page)
+  message(paste("Gathering results from:", url))
+  req <- GET(url = url, config = add_headers(Authorization = paste("token", git_pat)))
   repo_dat <-
     jsonlite::fromJSON(httr::content(req, as = "text"), flatten = TRUE)
+  message(paste("... Gathered", nrow(repo_dat$items), "repositories."))
+  
   repo_df <-
-    dplyr::tibble(repo_dat$items) %>% dplyr::select(name, html_url)
+    tibble(repo_dat$items) %>%
+    select(full_name, homepage, html_url, description, private) %>%
+    separate(full_name, into = c("org", "name"), sep = "/") %>%
+    
+    # Collapse topics so they can be printed
+    bind_cols(tibble(topics = unlist(
+      lapply(repo_dat$items$topics, paste, collapse = ", ")
+    ))) %>%
+
+    # Drop private repos and remove org column
+    filter(!(private)) %>%
+    select(!c(private, org)) %>%
+    
+    # # Rearrange columns
+    relocate(description, .before = topics) %>%
+    
+    # Keep only those with homepages and descriptions
+    filter(!(is.na(homepage)), homepage != "",!(is.na(description))) %>% 
   
-  # Create an artifact file containing the AnVIL repos, else write an empty file
-  if (!dir.exists("resources")) {
-    dir.create("resources")
-  }
-  if (nrow(repo_df) > 0) {
-    readr::write_tsv(repo_df, file.path('resources', 'AnVIL_repos.tsv'))
-  } else {
-    readr::write_tsv(tibble(), file.path('resources', 'AnVIL_repos.tsv'))
-  }
+    # Keep only AnVIL related content
+    mutate(is_anvil = str_detect(topics, "anvil")) %>% 
+    filter(is_anvil) %>% 
+    select(!(is_anvil))
   
+  full_repo_df <- rbind(full_repo_df, repo_df) %>%
+    dplyr::arrange(name)
+}
+
+# --------- Save the collection ---------
+
+# Create an artifact file containing the repos, else write an empty file
+if (!dir.exists("resources")) {
+  dir.create("resources")
+}
+if (nrow(full_repo_df) > 0) {
+  readr::write_tsv(full_repo_df, file.path('resources', 'AnVIL_repos.tsv'))
 } else {
-  message(paste("API request failed!"))
-  if (!dir.exists("resources")) {
-    dir.create("resources")
-  }
   readr::write_tsv(tibble(), file.path('resources', 'AnVIL_repos.tsv'))
 }
+
